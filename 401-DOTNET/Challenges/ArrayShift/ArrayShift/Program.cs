022c21a3b075f402eb61efd29286c625ad95a5e9
Bryant Davis <bryant.davis@hotmail.com>
2020-07-12 17:03:13 -0700

    Code Challenge2 finished

---
+++ 401-DOTNET/Challenges/ArrayShift/ArrayShift/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ArrayShift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertShiftArray(new int[] { 2, 4, 6, 8 }, 5);
+            InsertShiftArray(new int[] { 4, 8, 15, 23, 42 }, 16);
+
+        }
+        /// <summary>
+        /// Inserts a new number into the middle of the array
+        /// </summary>
+        /// <param name="currentArray">Is the current array of numbers</param>
+        /// <param name="insertedNumber">Is the number to be inserted in the middle of the current array</param>
+        /// <returns>A new array with the new number in the middle of the array</returns>
+        static int[] InsertShiftArray(int[] currentArray, int insertedNumber)
+        {
+            int[] newArray = new int[currentArray.Length + 1];
+            int insert = newArray.Length / 2;
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                if(i == insert)
+                {
+                    newArray[i] = insertedNumber;
+                }else if(i > insert)
+                {
+                    newArray[i] = currentArray[i - 1];
+                }
+                else 
+                {
+                    newArray[i] = currentArray[i];
+                }
+            }
+            string numbers = "";
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                numbers += $"{newArray[i]},";
+            }
+            Console.WriteLine($"The new array is {numbers}");
+            return newArray;
+        }
+    }
+}
---
+++ 401-DOTNET/Challenges/ArrayShift/README.md
@@ -0,0 +1,50 @@
+# Daily Code Challenge README Example
+
+## Perfect Sequence
+*Author: Bryant Davis*
+
+---
+
+### Problem Domain
+***[Problem Domain for the Code Challenge]***
+
+Given an array and a number return a new array with the number in the middle.
+
+---
+
+### Inputs and Expected Outputs
+
+| Input | Expected Output |
+|Int Array | Int Array
+
+
+---
+
+### Big O
+
+
+| Time | Space |
+| :----------- | :----------- |
+| O(n) | O(n) |
+
+
+---
+
+
+### Whiteboard Visual
+***[Your Whiteboard Image]***
+![Final](final.jpg)
+
+
+---
+
+### Change Log
+***[The change log will list any changes made to the code base. This includes any changes from TA/Instructor feedback]***  
+1.4 Method finished and both arrays pass
+1.3: Refactored method to ensure 2nd array passes  
+1.2 Inital method created, first array passes
+1.1 Inital push
+
+---
+
+For more information on Markdown: https://www.markdownguide.org/cheat-sheet
