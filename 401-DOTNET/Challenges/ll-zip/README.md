dc5c2dc02085e6dc95ae126a0d459f8ca7269505
Bryant Davis <bryant.davis@hotmail.com>
2020-07-15 20:12:16 -0700

    finished code challenge zip link lists

---
+++ 401-DOTNET/Challenges/ll-zip/README.md
@@ -0,0 +1,53 @@
+# Daily Code Challenge README Example
+
+## Perfect Sequence
+*Author: Bryant Davis*
+
+---
+
+### Problem Domain
+***[Problem Domain for the Code Challenge]***
+
+Create a console app with a method that receives 2 different Link Lists that combines that 2 and returns the combined Link Lists Head.
+
+---
+
+### Inputs and Expected Outputs
+
+| Input | Expected Output |
+|2 Single Link Lists | Link Lists Head
+
+
+---
+
+### Big O
+
+
+| Time | Space |
+| :----------- | :----------- |
+| O(1n) | O(1n) |
+
+
+---
+
+
+### Whiteboard Visual
+***[Your Whiteboard Image]***
+![Final](final.jpg)
+
+
+---
+
+### Change Log
+***[The change log will list any changes made to the code base. This includes any changes from TA/Instructor feedback]***  
+1.7: All tests are passing and Method is complete - July 15 2020
+1.6: Created tests for the Zip Method and changed code to ensure it passed - July 15 2020
+1.5: Created Method that combines 2 Link Lists - July 15 2020
+1.4: Method finished and both arrays pass
+1.3: Refactored method to ensure 2nd array passes  
+1.2: Inital method created, first array passes
+1.1: Inital push
+
+---
+
+For more information on Markdown: https://www.markdownguide.org/cheat-sheet
---
+++ 401-DOTNET/Challenges/ll-zip/ll-zip/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using LLLibrary;
+
+namespace ll_zip
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        /// <summary>
+        /// Takes in 2 different Linked Lists. Combines the two Linked Lists into one and returns the Head.
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <returns>Returns the Head of the newly created Linked Lists</returns>
+        public static Node ZipLists(LinkedList first, LinkedList second)
+        {
+            LinkedList newLinkedList = new LinkedList();
+            newLinkedList.Current = newLinkedList.Head;
+            first.Current = first.Head;
+            second.Current = second.Head;
+
+            while (first.Current != null || second.Current != null)
+            {
+                if (first.Current != null && second.Current != null)
+                {
+                    newLinkedList.Insert(second.Current.Value);
+                    newLinkedList.Insert(first.Current.Value);
+                    second.Current = second.Current.Next;
+                    first.Current = first.Current.Next;
+                }
+                else if (first.Current != null && second.Current == null)
+                {
+                    newLinkedList.Insert(first.Current.Value);
+                    first.Current = first.Current.Next;
+                }
+                else if (first.Current == null && second.Current != null)
+                {
+                    newLinkedList.Insert(second.Current.Value);
+                    second.Current = second.Current.Next;
+                }
+                else
+                {
+                    first.Current = first.Current.Next;
+                    second.Current = second.Current.Next;
+                }
+            }
+            return newLinkedList.Head;
+        }
+    }
+}
