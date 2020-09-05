b56b8a0e5b1d7ad8c22b604df32489055aed6ed6
BryantDavis1986 <Bryant.Davis@hotmail.com>
2020-09-05 14:10:59 -0700

    hr warm up

---
+++ HackerRank/Interview Preperation Kit/Warm-Up/JumpingOnTheClouds/JumpingOnTheClouds/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace JumpingOnTheClouds
+{
+    public class Class1
+    {
+        static int jumpingOnClouds(int[] c)
+        {
+            int jumps = 0;
+            int i = 0;
+            while (i < c.Length - 1)
+            {
+                if (c.Length > i + 2 && c[i + 2] != 1)
+                {
+                    jumps++;
+                    i = i + 2;
+                }
+                else if (c[i + 1] != 1)
+                {
+                    jumps++;
+                    i++;
+                }
+
+            }
+            return jumps;
+        }
+    }
+}
