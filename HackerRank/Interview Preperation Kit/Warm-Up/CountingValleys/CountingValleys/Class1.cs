815fb3d22c4ce644428f88a8a048dd6f0a5d8ae6
BryantDavis1986 <Bryant.Davis@hotmail.com>
2020-09-05 13:27:07 -0700

    hr warm up

---
+++ HackerRank/Interview Preperation Kit/Warm-Up/CountingValleys/CountingValleys/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CountingValleys
+{
+    public class Class1
+    {
+        // Complete the countingValleys function below.
+        static int countingValleys(int n, string s)
+        {
+            int valleys = 0;
+            int level = 0;
+
+            foreach(char item in s)
+            {
+                if(item == 'D')
+                {
+                    level--;
+                }
+                if(item == 'U')
+                {
+                    level++;
+                }
+                if(level == 0 && item == 'U')
+                {
+                    valleys++;
+                }
+            }
+            return valleys;
+
+        }
+
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string s = Console.ReadLine();
+
+            int result = countingValleys(n, s);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
