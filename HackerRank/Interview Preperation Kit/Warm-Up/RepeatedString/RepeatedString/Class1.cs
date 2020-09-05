78522279759db1007e7d0656b15d007f20374846
BryantDavis1986 <Bryant.Davis@hotmail.com>
2020-09-05 15:02:38 -0700

    warm up

---
+++ HackerRank/Interview Preperation Kit/Warm-Up/RepeatedString/RepeatedString/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RepeatedString
+{
+    public class Class1
+    {
+        static long repeatedString(string s, long n)
+        {
+            var count = 0L;
+            foreach (var letter in s)
+            {
+                if (letter == 'a')
+                    count++;
+            }
+
+            var possibleStringRepeatitions = n / s.Length;
+            count *= possibleStringRepeatitions;
+            var offsetStringLength = n % s.Length;
+
+            for (int i = 0; i < offsetStringLength; i++)
+            {
+                if (s[i] == 'a')
+                    count++;
+            }
+            return count;
+        }
+    }
+}
