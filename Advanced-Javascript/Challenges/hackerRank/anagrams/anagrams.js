35dfa437a377d53b2fdace8b9c5c5054e9664811
BryantDavis1986 <Bryant.Davis@hotmail.com>
2020-12-30 15:29:58 -0800

    hacker rank problem solving anagram

--- Advanced-Javascript/Challenges/hackerRank/anagrams/anagrams.js
+++ Advanced-Javascript/Challenges/hackerRank/anagrams/anagrams.js
@@ -16,4 +16,14 @@ return firstCopy.length + b.length
 
 }
 
+function anagram(s) {
+    const len = s.length
+        if(len % 2 == 1) return -1;
+        let sub1 = s.substring(0,len/2), sub2 = s.substring(len/2,len);
+        let count = 0;
+        sub2.split('').filter(x=> sub1.includes(x) ? (sub1 = sub1.replace(x,'')) : count++);
+        return count;
+    }
+    
+
 module.exports = makeAnagram;
